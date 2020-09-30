35407f535ac848365d268da43352b4ab8a382580
KD <info@appon.io>
2020-09-30 17:59:45 +0500

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SimpleSign
                 </div>
 
                 <div class="links">
